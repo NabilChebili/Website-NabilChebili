014e447bd0b827f510f4842435369c36a271d19e
Nabil Chebili <nabil.chebili.ext@suez.com>
2026-01-29 17:00:30 +0100

    feat: integrate sitemap functionality and enhance SEO metadata across multiple pages

--- portfolio-photo/astro.config.mjs
+++ portfolio-photo/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',   // important pour Azure Static Web Apps
   site: 'https://www.nabilchebili.fr', // facultatif, utile pour le SEO
+	integrations: [sitemap()],
 });
